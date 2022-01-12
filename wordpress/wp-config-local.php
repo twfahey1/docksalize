0c4d35ad66d6c08914da161fa4176ce73769b9a0
Tyler Fahey <tyler@tylerfahey.com>
2022-01-12 10:10:12 -0600

    Add initial wp-init command and proper config

--- wordpress/wp-config-local.php
+++ wordpress/wp-config-local.php
@@ -85,6 +85,3 @@ define( 'WP_DEBUG', false );
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
-
-/** Sets up WordPress vars and included files. */
-require_once( ABSPATH . 'wp-settings.php' );
